6eb941e46dc693a4cb7a62062ed878110e063b0e
ssreeram <ssreeram@d2.com>
2019-11-21 15:29:53 +0530

    #3141: modified code to check key existence before removing

--- hooks/tk-multi-publish2/silhouette/collector.py
+++ hooks/tk-multi-publish2/silhouette/collector.py
@@ -194,7 +194,7 @@ class SilhouetteSessionCollector(HookBaseClass):
 
         # also collect sequence files
         for key in self.sgtk.template_keys.values():
-            if isinstance(key, SequenceKey):
+            if isinstance(key, SequenceKey) and key.name in fields:
                 fields.pop(key.name)
 
         work_tmpl = publisher.get_template_by_name(work_path_template)
