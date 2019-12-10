4304adc759078766099aeee2c667482b0b45e3da
Tanay Dimri <tanay.dim@gmail.com>
2019-12-10 11:19:20 +0530

    Texture nodes created using loader would now rename the node based on your publish name (#168)
    * Texture nodes created using loader would now rename the node based on your publish name

--- hooks/tk-multi-loader2/tk-maya_actions.py
+++ hooks/tk-multi-loader2/tk-maya_actions.py
@@ -179,6 +179,22 @@ class CustomMayaActions(HookBaseClass):
     ##############################################################################################################
     # helper methods which can be subclassed in custom hooks to fine tune the behaviour of things
 
+    def rename_file_node(self, sg_publish_data, file_node):
+        """
+        This function would facilitate renaming of a given file_node based on the entity name
+        available in sg_publish_info
+
+        :param sg_publish_data: (dict) Shotgun data dictionary with all the standard publish fields.
+        :param file_node: (uniicode)   File Node that has to be renamed.
+        :return: (uniicode)            New file node name after renaming.
+        """
+        # Fetching the map type name from the file name at the path
+        entity_name = sg_publish_data["entity"]["name"]
+        pub_name = sg_publish_data["name"]
+        file_node_name = "{0}{1}".format(entity_name, pub_name.split(entity_name)[-1])
+        # Renaming the file node created to the map being loaded
+        return cmds.rename(file_node, file_node_name)
+
     def _create_texture_node_with_frames(self, path, sg_publish_data):
         """
         Create a file texture node for a texture
@@ -189,34 +205,46 @@ class CustomMayaActions(HookBaseClass):
         """
 
         # use mel command instead, since that creates the corresponding place2dtexture node with connections
+        # use mel command instead, since that creates the corresponding place2dtexture node with
+        # connections
         # file_node = cmds.shadingNode('file', asTexture=True)
         file_node = mel.eval('createRenderNodeCB -as2DTexture "" file ""')
 
         has_frame_spec, path = self._find_first_frame(path)
         cmds.setAttr("%s.fileTextureName" % file_node, path, type="string")
-
         if has_frame_spec:
             # setting the frame extension flag will create an expression to use
             # the current frame.
             cmds.setAttr("%s.useFrameExtension" % (file_node,), 1)
+
+        # Overriding the file_node variable to return the newly assigned name
+        file_node = self.rename_file_node(sg_publish_data, file_node)
+
         return file_node
 
     def _create_texture_node(self, path, sg_publish_data):
         """
         Create a file texture node for a texture
-        
+
         :param path:             Path to file.
         :param sg_publish_data:  Shotgun data dictionary with all the standard publish fields.
         :returns:                The newly created file node
         """
 
         # use mel command instead, since that creates the corresponding place2dtexture node with connections
+        # use mel command instead, since that creates the corresponding place2dtexture node with
+        # connections
         # file_node = cmds.shadingNode('file', asTexture=True)
         file_node = mel.eval('createRenderNodeCB -as2DTexture "" file ""')
 
         has_frame_spec, path = self._find_first_frame(path)
+
         # use the first frame instead of %04d, else maya errors out with "File Doesn't exist".
         cmds.setAttr("%s.fileTextureName" % file_node, path, type="string")
+
+        # Overriding the file_node variable to return the newly assigned name
+        file_node = self.rename_file_node(sg_publish_data, file_node)
+
         return file_node
 
     def _create_udim_texture_node(self, path, sg_publish_data):
