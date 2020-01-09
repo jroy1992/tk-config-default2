7231923f42dc8afe00edcefed08ffbf028e22335
ssreeram <ssreeram@d2.com>
2020-01-09 10:27:26 +0530

    #3942: switch sg-file_save context based on selected project in nuke studio

--- hooks/tk-multi-workfiles2/scene_operation_tk-nuke.py
+++ hooks/tk-multi-workfiles2/scene_operation_tk-nuke.py
@@ -331,10 +331,20 @@ class SceneOperation(HookClass):
         """
         import hiero
 
+        engine = sgtk.platform.current_engine()
+        engine_context = engine.context
+
         if operation == "current_path":
             # return the current script path
             project = self._get_current_hiero_project()
             curr_path = project.path().replace("/", os.path.sep)
+            if curr_path:
+                # switch file-save window context to selected project context
+                project = self._get_current_hiero_project()
+                tag_object = self.get_tag_object(project)
+                context_str = tag_object.note()
+                selected_project_context = engine_context.deserialize(context_str)
+                self.parent.change_context(selected_project_context)
             return curr_path
 
         elif operation == "open":
@@ -353,7 +363,11 @@ class SceneOperation(HookClass):
             hiero.core.events.sendEvent("kBeforeProjectLoad", None)
 
             # open the specified script
-            hiero.core.openProject(file_path.replace(os.path.sep, "/"))
+            project = hiero.core.openProject(file_path.replace(os.path.sep, "/"))
+
+            # store project context in tag.note()
+            context_str = engine_context.serialize(engine_context)
+            self.add_context_to_project(project, context_str)
 
         elif operation == "save":
             # save the current script:
@@ -378,6 +392,40 @@ class SceneOperation(HookClass):
         # call the task status updates
         return super(SceneOperation, self).execute(operation, file_path, context, parent_action, file_version, read_only,
                                             **kwargs)
+
+    def get_tag_name(self, project):
+        """
+        return selected project tag name
+        :param project:
+        :return:
+        """
+        return "{}_tag".format(project.name().split(".")[0])
+
+    def add_context_to_project(self, project, context_str):
+        import hiero
+        """
+        Add context to project(hrox file) using Tags to store serialized project's context as note
+        :param project:
+        :param context_str:
+        :return:
+        """
+        # add tag if doesn't link to respective project
+        tag_object = self.get_tag_object(project)
+        if not tag_object:
+            tag = hiero.core.Tag(self.get_tag_name(project))
+            tag.setNote(context_str)
+            project.tagsBin().addItem(tag)
+
+    def get_tag_object(self, project):
+        """
+        return tag object for selected project
+        :param project:
+        :return:
+        """
+        for tag_item in project.tagsBin().items():
+            if tag_item.name() == self.get_tag_name(project):
+                return tag_item
+        return
 
 
 def _update_save_menu_items(project):
